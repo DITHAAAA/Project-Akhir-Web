1b41d32b8cc4eb366de2154ff95aa7cf17d5ded1
unknown <raysblue316@gmail.com>
2023-05-07 21:22:35 +0800

    Push PA C2 KEL 3

---
+++ footer.php
@@ -0,0 +1,31 @@
+
+	<footer style="border-top: 4px solid #ff8680;  ">
+		<div class="container" style="padding-bottom: 50px;">
+			<div class="row">
+				<div class="col-md-4">
+					<h3 style="color: #19376D"><b>BAKERY</b></h3>
+					<p>Jl. Gunung Kelua</p>
+					<p><i class="glyphicon glyphicon-earphone"></i> +6287804616097</p>
+					<p><i class="glyphicon glyphicon-envelope"></i> bakery@gmail.com</p>
+				</div>
+				<div class="col-md-4">
+					<h5><b>MENU</b></h5>
+					<p><a href=""  style="color: #000">Produk</a></p>
+					<p><a href=""  style="color: #000">Tentang kami</a></p>
+					<p><a href=""  style="color: #000">Hubungi Kami</a></p>
+				</div>
+
+				<div class="col-md-4">
+					
+				</div>
+			</div>
+
+		</div>
+
+		<div class="copy" style="background-color: #19376D; padding: 5px; color: #fff; text-align: center;">
+			<span>Copyright&copy; C2 Kelompok 3</span>
+		</div>
+	</footer>
+
+</body>
+</html>
